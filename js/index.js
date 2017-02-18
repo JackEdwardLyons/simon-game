2deb68f2143931097ae4f320d979dbfa0baf3553
JackEdwardLyons <jack.edward.lyons@gmail.com>
2017-02-18 20:44:42 +1000

    adding js logic

--- js/index.js
+++ js/index.js
@@ -1,47 +1,103 @@
 
-
+/* 
+ * Set variables
+ ----------------------*/
 let   startButton   = document.querySelector('#start-button'),
       restartButton = document.querySelector('#restart-button'),
-      normalMode    = document.querySelector('#normal-mode'),
-      strictMode    = document.querySelector('#strict-mode'),
+      gameModes     = document.querySelectorAll('.game-mode'),
       simonTiles    = document.querySelectorAll('.simon-btn');
 
 
-
+/* 
+ * Game configurations
+ ------------------------*/
 let gameConfig = {
   score: 0,
-  computerMoves: [],
-  humanMoves: [],
-  gameMode: null
+  moves: {
+    computer: [],
+    human: []
+  },
+  mode: null,
+  gameStarted: false
 }
 
+
+/* 
+ * Gameplay Logic
+ ----------------------*/
+function startGame() {
+  if (gameConfig.mode !== null) {
+    setMessage("It's your turn!");
+    playSequence();
+  } 
+  alert('Please pick a game mode to begin!');
+}
+
+function restartGame() {
+  
+}
+
+function humanError() {
+  
+}
+
+function playSequence() {
+  
+}
+
+
+
+/* 
+ * Click Logic 
+ ----------------------*/
+function humanClick(e) {
+  const clickedTile = e.target.offsetParent
+  buzz(clickedTile);
+}
+
+function computerClick() {
+  buzz(randomTile(simonTiles));
+}
+
+function getGameMode(e) {
+  console.log(e.target.offsetParent.id);
+  return (e.target.offsetParent.id) === 'normal-mode'
+    ? gameConfig.mode = 'normal'
+    : gameConfig.mode = 'strict'
+}
+
+
+
+
+/* 
+ * Utility Functions 
+ ----------------------*/
 function randomTile(tiles) {
   const index = Math.floor( Math.random() * tiles.length );
   const tile = tiles[index];
-
-  console.log(tile);
+  return tile;
 }
-
 
 function buzz(tile) {
   const audio = tile.children[0];
-  console.log(audio);
   // reset audio play on each click
   audio.currentTime = 0;
   audio.play();
-  tile.classList.add('js-click');
   
+  tile.classList.add('js-click');
   setTimeout(() => {
     tile.classList.remove('js-click');
   }, 400)
-  
 }
 
-function registerClick(e) {
-  buzz(e.target.offsetParent);
+function setMessage(msg) {
+  let messageBox = document.getElementById('message-box');
+  messageBox.textContent = msg;
 }
 
-
-
-
-simonTiles.forEach(tile => tile.addEventListener('click', registerClick));
+/* 
+ * Event Listeners 
+ ----------------------*/
+startButton.addEventListener('click', startGame);
+gameModes.forEach(modeButton => modeButton.addEventListener('click', getGameMode));
+simonTiles.forEach(tile => tile.addEventListener('click', humanClick));
